7f06d06a4443b82ffed59f1a622325fa6db7c829
YeongKwonMo <momoci99@gmail.com>
2023-08-21 15:35:58 +0900

    chore:alias 변

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@src", replacement: path.resolve(__dirname, "src") },
+      { find: "@pages", replacement: path.resolve(__dirname, "src/pages") },
       {
         find: "@components",
         replacement: path.resolve(__dirname, "src/components"),

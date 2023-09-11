44a784d5ed687982d3acb8ce96047e40c8d5b1ba
YeongKwonMo <momoci99@gmail.com>
2023-09-11 17:29:39 +0900

    feat:카드 컴포넌트 추가 및 todoItem 스타일링

---
+++ src/components/Card.tsx
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div<{
+  size: "default" | "small" | "other";
+}>`
+  width: 100%;
+  height: 100%;
+
+  border-radius: 4px;
+  background: ${(props) => {
+    return props.theme.colors.neutral.white;
+  }};
+
+  .header {
+    border-radius: 8px 8px 0px 0px;
+    background-color: ${(props) => {
+      return props.theme.colors.primary.light;
+    }};
+
+    height: 36px;
+    padding: 0px 24px;
+    display: flex;
+    align-items: center;
+  }
+
+  .body {
+    ${(props) => {
+      if (props.size === "default") {
+        return `
+        padding: 24px;
+        box-shadow: 0px 4px 40px 0px rgba(15, 22, 58, 0.08);
+        border-radius: 4px;
+        background: #FFF;
+      `;
+      } else if (props.size === "small") {
+        return `
+        padding: 12px;
+        box-shadow: 0px 4px 4px 0px rgba(11, 26, 40, 0.04);
+        border-radius: 4px;
+        background: #FFF;
+      `;
+      } else if (props.size === "other") {
+        return `
+        padding: 0px;
+        box-shadow: 0px -4px 80px 0px rgba(2, 41, 100, 0.10);
+        border-radius: 8px;
+        background: #FFF;
+
+      `;
+      }
+    }}
+  }
+`;
+
+interface CardProps {
+  size?: "default" | "small" | "other";
+
+  header?: React.ReactNode;
+  children: React.ReactNode;
+  footer?: React.ReactNode;
+}
+
+const Card = (props: CardProps) => {
+  const { size } = props;
+  return (
+    <Wrapper size={size ? size : "default"}>
+      <div className="header">{props.header}</div>
+      <div className="body">{props.children}</div>
+      <div className="footer">{props.footer}</div>
+    </Wrapper>
+  );
+};
+
+export default Card;
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -3,6 +3,34 @@ import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { removeTodo, toggleCompleteStatus } from "@src/store/slices/todoSlice";
 import { RootState } from "@src/store";
+import Card from "@src/components/Card";
+
+import styled from "styled-components";
+
+const Wrapper = styled.li`
+  list-style: none;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .title {
+    color: ${(props) => {
+      return props.theme.colors.neutral.white;
+    }};
+
+    font-family: Noto Sans KR;
+    font-size: ${(props) => {
+      return props.theme.fontSizes.small;
+    }};
+
+    font-weight: ${(props) => {
+      return props.theme.fontWeights.light;
+    }};
+    line-height: normal;
+  }
+`;
+
 const TodoItem = ({ todo }: { todo: TodoItemType }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -12,76 +40,83 @@ const TodoItem = ({ todo }: { todo: TodoItemType }) => {
   });
 
   return (
-    <li>
-      <span>{todo.title}</span>
-      <span>{todo.description}</span>
-      <span>{todo.creationDate}</span>
-      <span>{todo.isCompleted}</span>
-      카테고리
-      <ul>
-        {todo.categoryIds.map((id) => {
-          return (
-            <li key={id}>
-              {todoCategories.find((category) => category.id === id)?.name}
-            </li>
-          );
-        })}
-      </ul>
-      <button
-        type="button"
-        onClick={() => {
-          navigate(`/updateTodo/${todo.id}`);
-        }}
-      >
-        수정하기
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          if (!todo) return;
-          fetch(`http://localhost:3000/todos/${todo.id}`, {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              id: todo.id,
-              title: todo.title,
-              description: todo.description,
-              creationDate: todo.creationDate,
-              isCompleted: !todo.isCompleted,
-            }),
-          })
-            .then(() => {
-              dispatch(
-                toggleCompleteStatus({ id: todo.id, flag: !todo.isCompleted })
+    <Wrapper>
+      <Card header={<p className="title">{todo.title}</p>}>
+        <span>{todo.description}</span>
+
+        <div>
+          <span>{todo.creationDate}</span>
+          <span>{todo.isCompleted}</span>
+          카테고리
+          <ul>
+            {todo.categoryIds.map((id) => {
+              return (
+                <li key={id}>
+                  {todoCategories.find((category) => category.id === id)?.name}
+                </li>
               );
-            })
-            .catch((reason) => {
-              console.error(reason);
-            });
-        }}
-      >
-        {todo.isCompleted ? "완료됨" : "진행예정"}
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          if (!todo) return;
-          fetch(`http://localhost:3000/todos/${todo.id}`, {
-            method: "DELETE",
-          })
-            .then(() => {
-              dispatch(removeTodo(todo.id));
-            })
-            .catch((reason) => {
-              console.error(reason);
-            });
-        }}
-      >
-        삭제
-      </button>
-    </li>
+            })}
+          </ul>
+          <button
+            type="button"
+            onClick={() => {
+              navigate(`/updateTodo/${todo.id}`);
+            }}
+          >
+            수정하기
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              if (!todo) return;
+              fetch(`http://localhost:3000/todos/${todo.id}`, {
+                method: "PUT",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  id: todo.id,
+                  title: todo.title,
+                  description: todo.description,
+                  creationDate: todo.creationDate,
+                  isCompleted: !todo.isCompleted,
+                }),
+              })
+                .then(() => {
+                  dispatch(
+                    toggleCompleteStatus({
+                      id: todo.id,
+                      flag: !todo.isCompleted,
+                    })
+                  );
+                })
+                .catch((reason) => {
+                  console.error(reason);
+                });
+            }}
+          >
+            {todo.isCompleted ? "완료됨" : "진행예정"}
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              if (!todo) return;
+              fetch(`http://localhost:3000/todos/${todo.id}`, {
+                method: "DELETE",
+              })
+                .then(() => {
+                  dispatch(removeTodo(todo.id));
+                })
+                .catch((reason) => {
+                  console.error(reason);
+                });
+            }}
+          >
+            삭제
+          </button>
+        </div>
+      </Card>
+    </Wrapper>
   );
 };
 
